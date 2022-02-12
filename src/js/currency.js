811361cf0a2dbd5923672e731468adcf6c6b2622
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-02-12 07:35:54 -0800

    Add additional error messages for specific errors

--- src/js/currency.js
+++ src/js/currency.js
@@ -6,6 +6,18 @@ export default class ExchangeRate {
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
+        } else if (this.status === 404) {
+          reject("404 Not Found: Incorrect Currency");
+        } else if (this.status === 403) {
+          reject("403 Forbidden: Incorrect API Key Entered");
+        } else if (this.status === 401) {
+          reject("401 Unauthorized: API Key Has Not Been Authenticated");
+        } else if (this.status === 400) {
+          reject("403 Forbidden: You Do Not Have Permission To Access URL");
+        } else if (this.status === 429) {
+          reject(
+            "429 Too Many API Requests: Increase Subscription Plan Or Contact Admin"
+          );
         } else {
           reject(request.response);
         }
