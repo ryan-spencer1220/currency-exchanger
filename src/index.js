ceab3e728c21560d2f9e15e9d95ede1284f6d5f2
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-02-19 19:34:14 -0800

    BUGFIX: Error handling issue

--- src/index.js
+++ src/index.js
@@ -3,87 +3,42 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import ExchangeRate from "./js/currency.js";
-import ExchangeRateToUSD from "./js/currency-to-USD.js";
+
+function clearFields() {
+  $(".display").text("");
+  $(".amount").text("");
+  $(".show-errors").text("");
+}
+
+function getElements(response) {
+  if (response.conversion_rate) {
+    let currentExchangeRate =
+      Math.round((response.conversion_rate + Number.EPSILON) * 100) / 100;
+    let amount = $("#amount").val();
+    $(".display").text(`${currentExchangeRate}`);
+    $(".multiply").text(`${currentExchangeRate * amount}`);
+  } else {
+    $(".display").text("Error");
+    $(".multiply").text("N/A");
+    $(".show-errors").text(
+      `There was an error! Due to a ${response} error, your request could not be processed. Please review API key and selected currency!`
+    );
+  }
+}
+
+async function getExchangeRates(firstCurrency, secondCurrency) {
+  const response = await ExchangeRate.getCurrency(
+    firstCurrency,
+    secondCurrency
+  );
+  getElements(response, firstCurrency, secondCurrency);
+}
 
 $(document).ready(function () {
   $("#conversion-calculator").click(function () {
-    let currencyType = $("#currency-type").val();
-    let amount = $("#amount").val();
-    let promise = ExchangeRate.getCurrency();
-    promise.then(
-      function (response) {
-        const body = JSON.parse(response);
-        if (currencyType === "USD") {
-          $(".display").text(`$${body.conversion_rates.USD}`);
-          $(".multiply").text(`$${amount * body.conversion_rates.USD}`);
-        } else if (currencyType === "EUR") {
-          $(".display").text(`€${body.conversion_rates.EUR}`);
-          $(".multiply").text(`€${amount * body.conversion_rates.EUR}`);
-        } else if (currencyType === "JPY") {
-          $(".display").text(`¥${body.conversion_rates.JPY}`);
-          $(".multiply").text(`¥${amount * body.conversion_rates.JPY}`);
-        } else if (currencyType === "CAD") {
-          $(".display").text(`$${body.conversion_rates.CAD}`);
-          $(".multiply").text(`$${amount * body.conversion_rates.CAD}`);
-        } else if (currencyType === "MXN") {
-          $(".display").text(`$${body.conversion_rates.MXN}`);
-          $(".multiply").text(`$${amount * body.conversion_rates.MXN}`);
-        } else if (currencyType === "CNY") {
-          $(".display").text(`¥${body.conversion_rates.CNY}`);
-          $(".multiply").text(`¥${amount * body.conversion_rates.CNY}`);
-        } else {
-          $(".display").text(
-            "There was an error processing your request! 404 Not Found: Incorrect Currency"
-          );
-          $(".multiply").text("N/A");
-        }
-      },
-      function (error) {
-        $(".display").text(
-          `There was an error processing your request! ${error}`
-        );
-        $(".multiply").text("N/A");
-      }
-    );
-  });
-  $("#conversion-calculator-toUSD").click(function () {
-    let currencyType = $("#currency-type-toUSD").val();
-    let amount = $("#amount-toUSD").val();
-    let promise = ExchangeRateToUSD.getCurrencyToUSD(currencyType);
-    promise.then(
-      function (response) {
-        const body = JSON.parse(response);
-        if (currencyType === "USD") {
-          $(".display-toUSD").text(`$${body.conversion_rates.USD}`);
-          $(".multiply-toUSD").text(`$${amount * body.conversion_rates.USD}`);
-        } else if (currencyType === "EUR") {
-          $(".display-toUSD").text(`$${body.conversion_rates.USD}`);
-          $(".multiply-toUSD").text(`$${amount * body.conversion_rates.USD}`);
-        } else if (currencyType === "JPY") {
-          $(".display-toUSD").text(`$${body.conversion_rates.USD}`);
-          $(".multiply-toUSD").text(`$${amount * body.conversion_rates.USD}`);
-        } else if (currencyType === "CAD") {
-          $(".display-toUSD").text(`$${body.conversion_rates.USD}`);
-          $(".multiply-toUSD").text(`$${amount * body.conversion_rates.USD}`);
-        } else if (currencyType === "MXN") {
-          $(".display-toUSD").text(`$${body.conversion_rates.USD}`);
-          $(".multiply-toUSD").text(`$${amount * body.conversion_rates.USD}`);
-        } else if (currencyType === "CNY") {
-          $(".display-toUSD").text(`$${body.conversion_rates.USD}`);
-          $(".multiply-toUSD").text(`$${amount * body.conversion_rates.USD}`);
-        } else {
-          $(".display-toUSD").text(
-            `There was an error processing your request!`
-          );
-          $(".multiply-toUSD").text("N/A");
-        }
-      },
-      function (error) {
-        $(".display-toUSD").text(
-          `There was an error processing your request! ${error}`
-        );
-        $(".multiply-toUSD").text("N/A");
-      }
-    );
+    let userCurrencyOne = $("#currency-one").val();
+    let userCurrencyTwo = $("#currency-two").val();
+    clearFields();
+    getExchangeRates(userCurrencyOne, userCurrencyTwo);
   });
 });
--- src/js/currency.js
+++ src/js/currency.js
@@ -1,29 +1,15 @@
 export default class ExchangeRate {
-  static getCurrency() {
-    return new Promise(function (resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-      request.onload = function () {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else if (this.status === 404) {
-          reject("404 Not Found: Incorrect Currency");
-        } else if (this.status === 403) {
-          reject("403 Forbidden: Incorrect API Key Entered");
-        } else if (this.status === 401) {
-          reject("401 Unauthorized: API Key Has Not Been Authenticated");
-        } else if (this.status === 400) {
-          reject("403 Forbidden: You Do Not Have Permission To Access URL");
-        } else if (this.status === 429) {
-          reject(
-            "429 Too Many API Requests: Increase Subscription Plan Or Contact Admin"
-          );
-        } else {
-          reject(request.response);
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
+  static async getCurrency(firstCurrency, secondCurrency) {
+    try {
+      const response = await fetch(
+        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${firstCurrency}/${secondCurrency}`
+      );
+      if (!response.ok) {
+        throw Error(response.status);
+      }
+      return await response.json();
+    } catch (error) {
+      return error.message;
+    }
   }
 }
